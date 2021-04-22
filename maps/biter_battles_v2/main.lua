8f83a00c8777727732f5a11f9073c17e6b543255
Aleksander Mistewicz <a.mistewicz@gmail.com>
2021-04-22 18:10:33 +0200

    Use few, but big feeds instead

--- maps/biter_battles_v2/main.lua
+++ maps/biter_battles_v2/main.lua
@@ -67,17 +67,12 @@ local function on_entity_died(event)
 end
 
 local auto_feed_values = {
-    [16] = {0, 120, 0, 0, 0, 0, 0},
-    [17] = {0, 140, 0, 0, 0, 0, 0},
-    [18] = {0, 170, 0, 0, 0, 0, 0},
-    [19] = {0, 200, 0, 0, 0, 0, 0},
-    [20] = {0, 240, 0, 0, 0, 0, 0},
-    [21] = {0, 280, 0, 0, 0, 0, 0},
-    [23] = {0, 320, 0, 0, 0, 0, 0},
-    [25] = {0, 360, 0, 0, 0, 0, 0},
-    [27] = {0, 400, 0, 0, 0, 0, 0},
-    [29] = {0, 440, 0, 0, 0, 0, 0},
-    [31] = {0, 480, 0, 0, 0, 0, 0}
+    [20] = {0, 200, 0, 0, 0, 0, 0},
+    [40] = {0, 800, 0, 0, 0, 0, 0},
+    [60] = {0, 3000, 0, 0, 0, 0, 0},
+    [80] = {0, 0, 0, 100, 0, 0, 0},
+    [100] = {0, 0, 0, 100, 0, 0, 0},
+    [120] = {0, 0, 0, 100, 0, 0, 0}
 }
 
 local function auto_feed()
@@ -103,8 +98,8 @@ local function auto_feed()
             return
         end
     end
-    if minute > 33 then
-        feeding.auto_feed_biters("south_biters", "logistic-science-pack", 500, minute)
+    if minute > 140 then
+        feeding.auto_feed_biters("south_biters", "chemical-science-pack", 60, minute)
     end
 end
 
